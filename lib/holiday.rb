b7ccf1b3351703268679c266028a50a5eca517bd
Alan Golman <golman.alan@gmail.com>
2018-05-27 21:05:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,6 +32,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
+  end   
 end
 
 def all_holidays_with_bbq(holiday_hash)
